d7bfcdcd7e4642b0e649a8fd5cb2549698460341
Rustam <azimzadeh.rustam@gmail.com>
2024-08-03 16:08:07 +0400

    added a mask for coloring only trees in the NDVI color map

--- forests.py
+++ forests.py
@@ -31,23 +31,27 @@ def NDVI_channel(image):
     red_channel = image[0]
     nir_channel = image[3]
     ndvi_channel = (nir_channel - red_channel) / (nir_channel + red_channel + 1e-10)
+    forest_mask = (ndvi_channel > 0.3) & (ndvi_channel < 1)
+    ndvi_forest_map = np.where(forest_mask, ndvi_channel, np.nan)
 
     figure, axes = plt.subplots(1, 2, figsize=(14, 5))
 
     ax1 = axes[0]
-    color_map = ax1.imshow(ndvi_channel, cmap='RdYlGn', vmin=-1, vmax=1)
+    ax1.imshow(red_channel, cmap='Grays')
+    color_map = ax1.imshow(ndvi_forest_map, cmap='RdYlGn', vmin=-1, vmax=1)
     plt.colorbar(color_map, ax=ax1)
-    ax1.set_title('NDVI color map')
+    ax1.set_title('NDVI color map for assessing forest health')
     ax1.axis('off')
 
     ax2 = axes[1]
-    ndvi_data = ndvi_channel[(ndvi_channel > 0.3) & (ndvi_channel < 1)]
-    ndvi_data = ndvi_data.ravel()
-    ndvi_data_mean = ndvi_data.mean()
-    ndvi_data_standard = ndvi_data.std()
-    bins = np.histogram_bin_edges(ndvi_data, bins='scott')
+    ndvi_forest_pattern = ndvi_channel[forest_mask]
+    ndvi_forest_data = ndvi_forest_pattern.ravel()
+    ndvi_data_mean = np.mean(ndvi_forest_data)
+    ndvi_data_median = np.median(ndvi_forest_data)
+    ndvi_data_standard = np.std(ndvi_forest_data)
+    bins = np.histogram_bin_edges(ndvi_forest_data, bins='scott')
 
-    ax2.set_title('')
+    ax2.set_title('Forest health histogram')
     ax2.set_ylabel('Frequency', fontsize=10)
     ax2.set_xlabel(r'$NDVI = \frac{NIR - Red}{NIR + Red}$', fontsize=10)
     ax2.xaxis.set_major_formatter(my_formatter)
@@ -55,7 +59,8 @@ def NDVI_channel(image):
     ax2.xaxis.get_offset_text().set_size(FONT_SIZE)
     ax2.yaxis.get_offset_text().set_size(FONT_SIZE)
     ax2.tick_params(axis='both', labelsize=FONT_SIZE)
-    ax2.hist(ndvi_data, label='Forest health (0.3 > NDVI > 1)', alpha=0.7, histtype='stepfilled', bins=bins, color='Green')
+    ax2.hist(ndvi_forest_data, label='Forest health (0.3 < NDVI < 1)', alpha=0.7, histtype='stepfilled', bins=bins, color='Green')
+    ax2.plot([], [], ' ', label=f'Median: {ndvi_data_median:.3f}')
     ax2.plot([], [], ' ', label=f'Mean: {ndvi_data_mean:.3f}')
     ax2.plot([], [], ' ', label=f'Std Dev: {ndvi_data_standard:.3f}')
     ax2.legend(loc='best', fontsize=10, fancybox=False, edgecolor='black')
